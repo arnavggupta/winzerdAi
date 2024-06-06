69634850986fd9486ffa7f22208d6a56e0f9f20c
arnavgupta <arnavgupta295@gmail.com>
2024-06-06 14:47:59 +0530

    Adding React Icons

--- src/Components/DrawerSideBar.1.tsx
+++ src/Components/DrawerSideBar.1.tsx
@@ -1,15 +1,15 @@
 import React from 'react';
-
+import { CiMenuBurger } from "react-icons/ci";
 
 export default function DrawerSideBar({ val, setval }: any) {
     const handeldrawer=()=>{
         setval(!val);
       }
     return (
-        <div className='flex flex-col  font-bold text-black cursor-pointer' onClick={handeldrawer}>
-          
-{val ? <h4>Close</h4> : <h4>Open</h4>}
-
+        <div className='flex flex-col  font-bold text-black cursor-pointer w-8'>
+          <div  onClick={handeldrawer}  className='text-xl '>
+<CiMenuBurger/>
+</div>
         </div>
     );
 }
